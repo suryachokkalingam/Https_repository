5b761641014871bf4ddaee4768dc89df5c9137b3
surya <schokkalingam@tekvizion.com>
2025-03-11 19:24:10 +0530

    Revert "direct to web page"
    This reverts commit d32193ec86891b4e71556d5093ef22945e0e9983.

--- web_page.py
+++ web_page.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import webbrowser
-import threading
 
 app = Flask(__name__)
 
@@ -8,9 +6,5 @@ app = Flask(__name__)
 def home():
     return "<h1>Welcome to My Simple Web Page</h1><p>This is a basic web page using Python and Flask.</p>"
 
-def open_browser():
-    webbrowser.open_new("http://127.0.0.1:5000/")
-
 if __name__ == '__main__':
-    threading.Timer(1.25, open_browser).start()  # Waits a bit before opening
     app.run(debug=True)
